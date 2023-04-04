b9e09c3d5b52d83874e387f83df42ece6275a7a8
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-04-04 21:56:00 +0300

    added uuid

--- src/Components/NoteComp/Notes.js
+++ src/Components/NoteComp/Notes.js
@@ -2,10 +2,28 @@ import { React, useState, useEffect } from "react";
 import "../Styles/Note.css";
 import Note from "./Note";
 import Create from "./Create";
+import { v4 as uuid } from "uuid";
 
 function Notes() {
   const [notes, setNotes] = useState([]);
   const [inputText, setInputText] = useState("");
+
+  const textHandler = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const saveHandler = () => {
+    setNotes((prevState) => [
+      ...prevState,
+      {
+        id: uuid(),
+        text: inputText,
+      },
+    ]);
+
+    setInputText("");
+  };
+
   return (
     <div className="notes">
       <Note />
